3606ee023916d84bd3fcdb748466822677d267d2
JyothishmathiCV <cvjyothishmathi@gmail.com>
2020-01-18 21:56:14 +0530

    Distance Matrix calculated

--- distance.py
+++ distance.py
@@ -15,18 +15,21 @@ def haversine(lon1, lat1, lon2, lat2):
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a)) 
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    return c * r * 1000
+    return int(c * r * 1000) ##for meters
 
 def get_row_col(nrow,ncol,obj):
-    ob = obj["cellno"]
-    row = ob//ncol
-    col = ob%ncol
+    ob = int(obj["cellno"])
+    row = int(ob)//ncol
+    col = int(ob)%ncol
     return row,col
 
-data = { "nrow":20, "ncol":23, "children":[{"lat","long","cellno"}] }
+with open("./processing/master.json") as f:
+    data = f.read()
+
+# data = { "nrow":20, "ncol":23, "children":[{"lat","long","cellno"}] }
 data = json.loads(data)
-nrows = data["nrow"]
-ncols = data["ncol"]
+nrows = int(data["nrow"])
+ncols = int(data["ncol"])
 points = data["children"]
 
 dist_matrix = []
@@ -37,6 +40,7 @@ for i in range(0,len(points)):
     for j in range(0,len(points)):
         if i!=j:
             jrow,jcol = get_row_col(nrows,ncols,points[j])
-            dist_matrix[i].append(haversine(points[i]["lon"],points[i]["lat"],points[j]["lon"],points[j]["lat"]))
+            dist_matrix[i].append(haversine(float(points[i]["long"]),float(points[i]["lat"]),float(points[j]["long"]),float(points[j]["lat"])))
+        else:
+            dist_matrix[i].append(0)
 
-print(dist_matrix)
