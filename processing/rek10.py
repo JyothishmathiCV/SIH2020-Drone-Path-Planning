5d1036437e1dfe98070805ce6a4251db89bee215
mohanpierce99 <mohan@madstreetden.com>
2020-01-18 21:20:41 +0530

    JSON parse fix

--- processing/rek10.py
+++ processing/rek10.py
@@ -1,15 +1,15 @@
 import re
 import json
 
-master=[]
-with open("mohan.txt") as f:
+master={"nrow": 20 ,"ncol": 23 ,"children":[]}
+with open("raw.txt") as f:
     arr=f.read().split("\n")
     for items in arr:
         al=re.findall(r'[\-0\.-9]+\s*',items)
         if not len(al):
             break
         dicter={"lat":al[2],"long":al[3],"cellno":al[1]}
-        master.append(dicter)
+        master["children"].append(dicter)
     fr=open("master.json","w")
     fr.write(json.dumps(master))
     fr.close()
